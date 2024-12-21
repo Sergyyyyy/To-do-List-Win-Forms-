476d97b059bac60da3cd2da6cd8c040180abaec2
Sergio <Sergio@LAPTOP-T42O9IN1>
2024-12-21 21:41:22 +0800

    Add project files.

---
+++ To do List (Win Forms)/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace To_do_List__Win_Forms_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddTaskBtn = new System.Windows.Forms.Button();
+            this.RemoveTaskBtn = new System.Windows.Forms.Button();
+            this.InputTaskTextBox = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(68, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(312, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LIST OF TASKS";
+            // 
+            // AddTaskBtn
+            // 
+            this.AddTaskBtn.BackColor = System.Drawing.Color.SlateBlue;
+            this.AddTaskBtn.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddTaskBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AddTaskBtn.Location = new System.Drawing.Point(29, 148);
+            this.AddTaskBtn.Name = "AddTaskBtn";
+            this.AddTaskBtn.Size = new System.Drawing.Size(124, 45);
+            this.AddTaskBtn.TabIndex = 1;
+            this.AddTaskBtn.Text = "ADD TASK";
+            this.AddTaskBtn.UseVisualStyleBackColor = false;
+            this.AddTaskBtn.Click += new System.EventHandler(this.AddTaskBtn_Click);
+            // 
+            // RemoveTaskBtn
+            // 
+            this.RemoveTaskBtn.BackColor = System.Drawing.Color.Violet;
+            this.RemoveTaskBtn.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemoveTaskBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.RemoveTaskBtn.Location = new System.Drawing.Point(159, 148);
+            this.RemoveTaskBtn.Name = "RemoveTaskBtn";
+            this.RemoveTaskBtn.Size = new System.Drawing.Size(121, 45);
+            this.RemoveTaskBtn.TabIndex = 2;
+            this.RemoveTaskBtn.Text = "REMOVE TASK";
+            this.RemoveTaskBtn.UseVisualStyleBackColor = false;
+            this.RemoveTaskBtn.Click += new System.EventHandler(this.RemoveTaskBtn_Click);
+            // 
+            // InputTaskTextBox
+            // 
+            this.InputTaskTextBox.Font = new System.Drawing.Font("Bahnschrift", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTaskTextBox.Location = new System.Drawing.Point(29, 87);
+            this.InputTaskTextBox.Name = "InputTaskTextBox";
+            this.InputTaskTextBox.Size = new System.Drawing.Size(386, 40);
+            this.InputTaskTextBox.TabIndex = 4;
+            this.InputTaskTextBox.TextChanged += new System.EventHandler(this.InputTaskTextBox_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.SlateBlue;
+            this.listBox1.Font = new System.Drawing.Font("Bahnschrift", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 33;
+            this.listBox1.Location = new System.Drawing.Point(29, 210);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(386, 136);
+            this.listBox1.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Brown;
+            this.button1.Font = new System.Drawing.Font("Bahnschrift", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(286, 148);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 45);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(446, 368);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.InputTaskTextBox);
+            this.Controls.Add(this.RemoveTaskBtn);
+            this.Controls.Add(this.AddTaskBtn);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddTaskBtn;
+        private System.Windows.Forms.Button RemoveTaskBtn;
+        private System.Windows.Forms.TextBox InputTaskTextBox;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ To do List (Win Forms)/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace To_do_List__Win_Forms_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddTaskBtn_Click(object sender, EventArgs e)
+        {
+            string task = InputTaskTextBox.Text;
+
+            if (!String.IsNullOrEmpty(task))
+            {
+                listBox1.Items.Add(task);
+                InputTaskTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Error! Please add a task to the Text Box before clicking", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+        }
+
+        private void RemoveTaskBtn_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+            else
+            {
+                MessageBox.Show("Error! Please select a task to delete.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void InputTaskTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
